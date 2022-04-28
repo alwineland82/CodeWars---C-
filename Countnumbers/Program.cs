3a8ab85e2cd3addd56127094fc8fe91e0b406581
al_wineland <alwineland@yandex.ru>
2022-04-29 01:57:35 +1100

    Counnumbers

---
+++ Countnumbers/Program.cs
@@ -0,0 +1,30 @@
+﻿int[] CountPositivesSumNegatives(int[] input)
+{
+    int[] empty = new int[0];
+    int[] result = new int[2]; 
+    int count = 0;
+    int sum = 0;
+    if (input == null)
+      {
+      return empty;
+    }
+    foreach(int i in input)
+    {
+        if (i > 0)
+        {
+            count++;
+        }else if (i < 0)
+        {
+            sum += i;
+        }
+    }
+    if (input.Length == 0)
+    {
+        return empty;
+    }else
+    {
+        result[0] = count;
+        result[1] = sum;
+        return result;
+    }
+}
