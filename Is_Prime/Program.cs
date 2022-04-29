cbf63422a9fd1bcb1e3a3992ffc10b46e59d2620
al_wineland <alwineland@yandex.ru>
2022-04-29 17:52:03 +1100

    stolen primes

---
+++ Is_Prime/Program.cs
@@ -0,0 +1,16 @@
+﻿bool Is_Prime(int num)
+{
+    if(num == 1 | num <= 0)
+    {
+        return false;
+    }
+    for(int i = 2; i * i <= num; i++)
+    {
+        if(num % i == 0)
+        {
+            return false;
+        }
+    }return true;
+}
+
+Console.WriteLine(Is_Prime(49));
