481ecf916b6499b4f701027f969c0fbc2f95542c
al_wineland <alwineland@yandex.ru>
2022-04-30 16:39:13 +1100

    Split strings

---
+++ Split Strings/Program.cs
@@ -0,0 +1,23 @@
+﻿string[] Solution(string str)
+{
+    string[] res = {};
+    List<string> EmptyList = new List<string>();
+    if(str.Length % 2 != 0) str += "_";
+    string inter = "";
+    foreach(char i in str)
+    {
+        inter += i;
+        if(inter.Length == 2)
+        {
+            EmptyList.Add(inter);
+            inter = "";
+
+        } 
+    }
+    res = EmptyList.ToArray();
+    return res;
+}
+foreach(string i in Solution("dfgsdhsfghsf"))
+{
+    Console.WriteLine(i);
+}
