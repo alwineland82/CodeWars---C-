958506acf6957f192b93d5627dfc7a437df351b6
al_wineland <alwineland@yandex.ru>
2022-04-30 12:40:15 +1100

    shortest word

---
+++ FindShort/Program.cs
@@ -0,0 +1,15 @@
+﻿int FindShort(string s)
+{
+    string[] temp = s.Split(' ');
+    int res = 100;
+    foreach(string i in temp)
+    {
+        if(i.Length < res)
+        {
+            res = i.Length;
+        }
+    }
+    return res;
+}
+
+Console.WriteLine(FindShort("bitcoin take over the world maybe who knows perhaps"));
