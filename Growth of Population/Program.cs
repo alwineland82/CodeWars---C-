0329aefce3078f6379e6cc71efad3bd7a2924c80
al_wineland <alwineland@yandex.ru>
2022-05-01 22:05:46 +1100

    Population

---
+++ Growth of Population/Program.cs
@@ -0,0 +1,12 @@
+﻿int NbYear(int p0, double percent, int aug, int p) {
+        int count = 0;
+        while(p0 < p)
+          {
+          p0 = (int)(p0 + p0 * (percent / 100) + aug);
+          count += 1;
+        }
+      return count;
+    }
+
+
+Console.WriteLine(NbYear(1500, 5, 100, 5000));
