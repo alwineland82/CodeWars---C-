cf74cc46e339f23b8711e48526f14fa5e7d75180
al_wineland <alwineland@yandex.ru>
2022-05-02 13:23:50 +1100

    Longest

---
+++ Longest/Program.cs
@@ -0,0 +1,26 @@
+﻿string[] Longest(string a, string b)
+{
+    string common = a + b;
+    string[] res = SetSplitter(common);
+    Array.Sort(res);
+    return res;
+}
+Display(Longest("aretheyhere", "yestheyarehere"));
+
+void Display(string[] arr)
+    {
+        for (int i = 0; i < arr.Length; i++)
+        {Console.Write(arr[i] + " ");}
+        Console.WriteLine();
+    }
+string[] SetSplitter(string x)
+{
+    string[] array = new string[x.Length];
+    for(int i = 0; i <= x.Length - 1; i++)
+    {
+        if(!array.Any(n => n == Convert.ToString(x[i])))
+        {
+            array[i] = Convert.ToString(x[i]);
+        }
+    }return array;
+}
