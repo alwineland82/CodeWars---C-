9fc16493b243a25fe4a841ae755346480b406a0f
al_wineland <alwineland@yandex.ru>
2022-05-03 13:30:33 +1100

    3-5

---
+++ Multiples of 3 or 5/Program.cs
@@ -0,0 +1,13 @@
+﻿int Solution(int value) 
+{   
+     if(value < 0) return 0;
+     int sum = 0;
+     for(int i = 0; i < value; i++)
+     {
+         if(i % 3 == 0 || i % 5 == 0)
+         {
+             sum += i;
+         }
+     }return sum;
+}
+Console.WriteLine(Solution(10));
