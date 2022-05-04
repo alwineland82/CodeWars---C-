617b0f4707b7fcb1a7aa32c08de47a733cace30d
al_wineland <alwineland@yandex.ru>
2022-05-04 23:27:26 +1100

    wierd_case

---
+++ wierd_strings/Program.cs
@@ -0,0 +1,21 @@
+﻿string ToWeirdCase(string s)
+{
+    int count = 0;
+    string reply = "";
+    foreach(char i in s)
+    {
+        if(" ,.".Contains(i)) {count = 0; reply += i; continue;}
+        if(count % 2 == 0)
+        {
+            reply += Convert.ToString(i).ToUpper();
+        }else if((count % 2 != 0))
+        {
+            reply += Convert.ToString(i).ToLower();
+        }
+        else
+        {
+            reply += i;
+        }count += 1;
+    }return reply;
+}
+Console.WriteLine(ToWeirdCase("Boy")); /// ThIs Is A TeSt
