8eb5510a84f2518f1b94396d4651fcfccfcc7879
al_wineland <alwineland@yandex.ru>
2022-05-06 00:50:50 +1100

    orderWeight

---
+++ DeleteNth/Program.cs
@@ -0,0 +1,17 @@
+﻿int[] DeleteNth(int[] arr, int x)
+{
+    int Count(List<int> arr, int n){int count = 0; foreach(int i in arr){if(i == n) count += 1;} return count;}
+    int[] res = {};
+    List<int> EmptyList = new List<int>();
+    foreach(int i in arr)
+    {
+        if(Count(EmptyList, i) < x)
+        {
+            EmptyList.Add(i);
+        }
+    }res = EmptyList.ToArray();
+    return res;
+}
+
+
+Console.WriteLine(DeleteNth(new int[] {1,1,3,3,7,2,2,2,2}, 3));
---
+++ orderWeight/Program.cs
@@ -0,0 +1,34 @@
+﻿string orderWeight(string strng)
+{
+    string[] strngList = strng.Split(' ');
+    Array.Sort(strngList);
+    int Sum(string num){int sum = 0; foreach(var i in num){sum += Convert.ToInt32(Convert.ToString(i));}return sum;}
+    string reply = "";
+    int count = 0;
+    while (count < strngList.Length)
+    {
+        int min = 10000000;
+        string adder = "";
+        foreach(var i in strngList)
+        {
+            if(min > Sum(i)) {min = Sum(i); adder = i;}
+        }reply += adder + " ";
+        strngList[Array.IndexOf(strngList, adder)] = "99999999";
+        count += 1;
+
+    }return reply.TrimEnd();
+}
+Console.WriteLine(orderWeight("2000 10003 1234000 44444444 9999 11 11 22 123"));
+
+
+int Sum(string num){int sum = 0; foreach(var i in num){sum += Convert.ToInt32(Convert.ToString(i));}return sum;}
+
+int[] array = {1, 2, 3, 4, 5};
+array[0] = 99999;
+Console.WriteLine(Array.IndexOf(array, 99999));
+
+///Console.WriteLine(Sum("2000"));
+///Console.WriteLine(Sum("11"));
+
+
+///List<string> result = names.Split(',').ToList();
