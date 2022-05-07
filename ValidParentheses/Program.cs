4b71d2ba743ccea8e41c9d760b1b3f4d510d3352
al_wineland <alwineland@yandex.ru>
2022-05-08 06:09:03 +1100

    ValidParentheses

---
+++ ValidParentheses/Program.cs
@@ -0,0 +1,14 @@
+﻿bool ValidParentheses(string input)
+{   
+    int res = 0;
+    foreach(var i in input)
+    {
+        if(i == '(') res += 1;
+        if(i == ')') res -= 1;
+        if(res < 0) return false;
+    }return res == 0;
+}
+
+string x = "((()))()";
+
+Console.WriteLine(ValidParentheses(x));    
